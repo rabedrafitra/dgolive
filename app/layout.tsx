1580e0da43d271e8c0e52ba8a5e607e263d5ef1a
rabedrafitra <lordjacyn@gmail.com>
2025-06-23 12:14:32 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Script src="/detect-os.js" strategy="afterInteractive" />
       </body>
     </html>
     </ClerkProvider>
