62fde66bc00152d52ff4e031737fd4a26cf6f153
rabedrafitra <lordjacyn@gmail.com>
2025-07-14 18:08:46 +0300

    Mise à jour des fichiers locaux

--- app/session/[liveId]/page.tsx
+++ app/session/[liveId]/page.tsx
@@ -492,7 +492,7 @@ const Page = ({ params }: { params: Promise<{ liveId: string }> }) => {
 
               <tr>
                 <th colSpan={8} className="text-lg text-center py-2">
-                  Coût d&aposentrée : {live?.purchasePrice ? `${live.purchasePrice.toLocaleString('fr-FR')} Ar` : 'N/A'}
+                  Coût Entrée: {live?.purchasePrice ? `${live.purchasePrice.toLocaleString('fr-FR')} Ar` : 'N/A'}
                 </th>
               </tr>
               <tr>
